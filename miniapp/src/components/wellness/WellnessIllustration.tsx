df62dd56a79fb1be8b2d060067a44e7f78b6c0fe
AlexLazarus99 <al3xlazarus@yandex.ru>
2026-06-01 21:42:58 +0300

    1

--- miniapp/src/components/wellness/WellnessIllustration.tsx
+++ miniapp/src/components/wellness/WellnessIllustration.tsx
@@ -1,24 +1,48 @@
-import type { BodyTypeId, DietId } from "../../data/wellness/catalog";
-import { DIETS } from "../../data/wellness/catalog";
-import { BodyTypePhoto } from "./BodyTypePhoto";
-import { DietPhoto } from "./DietPhoto";
-
-type IllustrationProps = {
-  id: BodyTypeId | DietId;
-  className?: string;
-};
-
-const BODY_TYPES = new Set<string>(["ectomorph", "mesomorph", "endomorph"]);
-const DIET_IDS = new Set<string>(DIETS);
-
-export function WellnessIllustration({ id, className = "" }: IllustrationProps) {
-  if (BODY_TYPES.has(id)) {
-    return <BodyTypePhoto id={id as BodyTypeId} className={className} />;
-  }
-
-  if (DIET_IDS.has(id)) {
-    return <DietPhoto id={id as DietId} className={className} />;
-  }
-
-  return null;
-}
+import type { BodyTypeId, DietId } from "../../data/wellness/catalog";
+
+import { DIETS } from "../../data/wellness/catalog";
+
+import { BodyTypePhoto } from "./BodyTypePhoto";
+
+import { DietPhoto } from "./DietPhoto";
+
+
+
+type IllustrationProps = {
+
+  id: BodyTypeId | DietId;
+
+  className?: string;
+
+};
+
+
+
+const BODY_TYPES = new Set<string>(["ectomorph", "mesomorph", "endomorph"]);
+
+const DIET_IDS = new Set<string>(DIETS);
+
+
+
+export function WellnessIllustration({ id, className = "" }: IllustrationProps) {
+
+  if (BODY_TYPES.has(id)) {
+
+    return <BodyTypePhoto id={id as BodyTypeId} className={className} />;
+
+  }
+
+
+
+  if (DIET_IDS.has(id)) {
+
+    return <DietPhoto id={id as DietId} className={className} />;
+
+  }
+
+
+
+  return null;
+
+}
+
